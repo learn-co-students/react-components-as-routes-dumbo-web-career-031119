f40416358d38a352ebc1bc6da49511f5a63a7245
Danielle Alba <danib.sgh@gmail.com>
2019-05-22 17:23:36 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 
 const Home = () => {
   return (
     <div>
-      <h1>Home!</h1>
+      <h1>☠︎ 🂱Home biatches🂱 ☠︎!</h1>
     </div>
   );
 };
 
-ReactDOM.render(
-  <Home />,
+ReactDOM.render((
+  <Router>
+    <Route path="/" render={Home} />
+  </Router>),
   document.getElementById('root')
 );
